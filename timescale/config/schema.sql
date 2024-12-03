42c0f10d6d38a39a69d372f751ec0be81a94daa9
korrupt <k0rrupt.dev@gmail.com>
2024-12-03 14:25:47 +0100

    feat: removed domain logic from schemafile

--- timescale/config/schema.sql
+++ timescale/config/schema.sql
@@ -1,16 +1,6 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE EXTENSION IF NOT EXISTS "postgis";
 
-CREATE TABLE IF NOT EXISTS domain (
-    id uuid DEFAULT uuid_generate_v4(),
-    slug character varying NOT NULL COLLATE pg_catalog."default",
-    name character varying NOT NULL collate pg_catalog."default",
-    description character varying NOT NULL COLLATE pg_catalog."default",
-
-    CONSTRAINT "PK_DOMAIN_ID" PRIMARY KEY (id),
-    CONSTRAINT "UQ_DOMAIN_SLUG" UNIQUE (slug)
-);
-
 CREATE TABLE IF NOT EXISTS device (
     id uuid DEFAULT uuid_generate_v4(),
     slug character varying COLLATE pg_catalog."default",
@@ -20,24 +10,6 @@ CREATE TABLE IF NOT EXISTS device (
     CONSTRAINT "PK_DEVICE_ID" PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS domain_device (
-    domain_id uuid NOT NULL,
-    device_id uuid NOT NULL,
-
-    slug character varying COLLATE pg_catalog."default",
-
-    CONSTRAINT "FK_DOMAIN_DEVICE_DOMAIN" FOREIGN KEY (domain_id)
-        REFERENCES public."domain" (id)
-        ON UPDATE NO ACTION
-        ON DELETE CASCADE,
-    
-    CONSTRAINT "FK_DOMAIN_DEVICE_DEVICE" FOREIGN KEY (device_id)
-        REFERENCES public."device" (id)
-        ON UPDATE NO ACTION
-        ON DELETE CASCADE,
-
-    CONSTRAINT "UQ_DOMAIN_DEVICE_SLUG" UNIQUE (domain_id, slug)
-);
 
 CREATE TABLE IF NOT EXISTS watcher (
     id uuid NOT NULL DEFAULT uuid_generate_v4(),
@@ -53,32 +25,6 @@ CREATE TABLE IF NOT EXISTS watcher (
         ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS DOMAIN_DEVICE_WATCHER (
-    domain_id uuid NOT NULL,
-    device_id uuid NOT NULL,
-    watcher_id uuid NOT NULL,
-
-    slug character varying NOT NULL,
-
-    CONSTRAINT "FK_DOMAIN_DEVICE_WATCHER_DOMAIN" FOREIGN KEY (domain_id)
-        REFERENCES public."domain" (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE CASCADE,
-
-    CONSTRAINT "FK_DOMAIN_DEVICE_WATCHER_DEVICE" FOREIGN KEY (device_id)
-        REFERENCES public."device" (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE CASCADE,
-
-    CONSTRAINT "FK_DOMAIN_DEVICE_WATCHER_WATCHER" FOREIGN KEY (watcher_id)
-        REFERENCES public."watcher" (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE CASCADE,
-
-    CONSTRAINT "PK_DOMAIN_DEVICE_WATCHER" PRIMARY KEY (domain_id, device_id, watcher_id),
-    CONSTRAINT "UQ_DOMAIN_DEVICE_WATCHER_SLUG" UNIQUE (domain_id, device_id, watcher_id, slug)
-);
-
 CREATE TABLE IF NOT EXISTS watcher_heartbeat (
     time TIMESTAMPTZ NOT NULL,
     data JSON DEFAULT '{}',
@@ -89,6 +35,64 @@ CREATE TABLE IF NOT EXISTS watcher_heartbeat (
         ON UPDATE NO ACTION
         ON DELETE CASCADE
 );
+
+-- CREATE TABLE IF NOT EXISTS domain (
+--     id uuid DEFAULT uuid_generate_v4(),
+--     slug character varying NOT NULL COLLATE pg_catalog."default",
+--     name character varying NOT NULL collate pg_catalog."default",
+--     description character varying NOT NULL COLLATE pg_catalog."default",
+
+--     CONSTRAINT "PK_DOMAIN_ID" PRIMARY KEY (id),
+--     CONSTRAINT "UQ_DOMAIN_SLUG" UNIQUE (slug)
+-- );
+
+-- CREATE TABLE IF NOT EXISTS domain_device (
+--     domain_id uuid NOT NULL,
+--     device_id uuid NOT NULL,
+
+--     slug character varying COLLATE pg_catalog."default",
+
+--     CONSTRAINT "FK_DOMAIN_DEVICE_DOMAIN" FOREIGN KEY (domain_id)
+--         REFERENCES public."domain" (id)
+--         ON UPDATE NO ACTION
+--         ON DELETE CASCADE,
+    
+--     CONSTRAINT "FK_DOMAIN_DEVICE_DEVICE" FOREIGN KEY (device_id)
+--         REFERENCES public."device" (id)
+--         ON UPDATE NO ACTION
+--         ON DELETE CASCADE,
+
+--     CONSTRAINT "UQ_DOMAIN_DEVICE_SLUG" UNIQUE (domain_id, slug)
+-- );
+
+
+-- CREATE TABLE IF NOT EXISTS DOMAIN_DEVICE_WATCHER (
+--     domain_id uuid NOT NULL,
+--     device_id uuid NOT NULL,
+--     watcher_id uuid NOT NULL,
+
+--     slug character varying NOT NULL,
+
+--     CONSTRAINT "FK_DOMAIN_DEVICE_WATCHER_DOMAIN" FOREIGN KEY (domain_id)
+--         REFERENCES public."domain" (id) MATCH SIMPLE
+--         ON UPDATE NO ACTION
+--         ON DELETE CASCADE,
+
+--     CONSTRAINT "FK_DOMAIN_DEVICE_WATCHER_DEVICE" FOREIGN KEY (device_id)
+--         REFERENCES public."device" (id) MATCH SIMPLE
+--         ON UPDATE NO ACTION
+--         ON DELETE CASCADE,
+
+--     CONSTRAINT "FK_DOMAIN_DEVICE_WATCHER_WATCHER" FOREIGN KEY (watcher_id)
+--         REFERENCES public."watcher" (id) MATCH SIMPLE
+--         ON UPDATE NO ACTION
+--         ON DELETE CASCADE,
+
+--     CONSTRAINT "PK_DOMAIN_DEVICE_WATCHER" PRIMARY KEY (domain_id, device_id, watcher_id),
+--     CONSTRAINT "UQ_DOMAIN_DEVICE_WATCHER_SLUG" UNIQUE (domain_id, device_id, watcher_id, slug)
+-- );
+
+
 
 SELECT create_hypertable('watcher_heartbeat','time');
 
