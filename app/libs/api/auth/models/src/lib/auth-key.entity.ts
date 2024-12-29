a4d213d23a201d6021de950c0ead10af99fcaa97
korrupt <k0rrupt.dev@gmail.com>
2024-12-29 14:33:50 +0100

    feat: switched auth key from device to watcher

--- app/libs/api/auth/models/src/lib/auth-key.entity.ts
+++ app/libs/api/auth/models/src/lib/auth-key.entity.ts
@@ -8,7 +8,7 @@ import {
   Unique,
   UpdateDateColumn,
 } from 'typeorm';
-import { DeviceEntity } from '@app/device-models';
+import { WatcherEntity } from '@app/device-models';
 
 @Entity('auth_key')
 @Unique(AuthkeyEntity.UQ_AUTH_KEY_KEY, ['key'])
@@ -36,7 +36,7 @@ export class AuthkeyEntity {
   @Column({ type: 'timestamptz', name: 'revoked_at', nullable: true })
   revoked_at?: Date;
 
-  @ManyToOne(() => DeviceEntity, { onDelete: 'CASCADE' })
-  @JoinColumn({ name: 'device_id' })
-  device_id!: string;
+  @ManyToOne(() => WatcherEntity, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'watcher_id' })
+  watcher_id!: string;
 }
